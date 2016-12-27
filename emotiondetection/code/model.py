7101e97981629a3ffab71cf64e1f4430b4349342
Qi Liu <liuq901@163.com>
2016-12-27 16:01:17 +0800

    using one vs rest multiclass classifier

--- emotiondetection/code/model.py
+++ emotiondetection/code/model.py
@@ -2,15 +2,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
 
-def get_model(name):
+def _get_raw_model(name):
     if name == 'logistic_regression':
-        return LogisticRegression()
+        return LogisticRegression(class_weight = 'balanced')
     elif name == 'naive_bayes':
         return GaussianNB()
     elif name == 'decision_tree':
-        return DecisionTreeClassifier()
+        return DecisionTreeClassifier(class_weight = 'balanced')
     elif name == 'SVM':
-        return SVC()
+        return SVC(class_weight = 'balanced')
     else:
         raise ValueError('No such model')
+
+def get_model(name):
+    model = _get_raw_model(name)
+    return OneVsRestClassifier(model)
